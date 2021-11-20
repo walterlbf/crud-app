ae73e56424511f7477093eca952ebe903fbbdbe2
walterlbf <walter.lbf.f@gmail.com>
2021-11-20 10:30:04 -0300

    reducer inicial configurado

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import reducer from "./itemReducer";
+
+const rootReducer = combineReducers({
+    reducer,
+});
+
+export default rootReducer;
---
+++ src/reducers/itemReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {};
+
+const reducer = (state = INITIAL_STATE, action) => {
+    switch (action.type){
+        case '':
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default reducer;
