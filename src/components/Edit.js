f8ea5b81d02c5ad2820639a2a404500dfc4a111f
walterlbf <walter.lbf.f@gmail.com>
2021-11-25 15:55:18 -0300

    componente Edit refatorado de class para arrow function e usado o hook useState para controlar o estado do componente

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,55 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { saveEdit } from '../actions';
 
-class Edit extends Component {
-    
-    constructor(props) {
-        super(props);
-        const { item } = this.props;
-        this.state = {
-            id: item.id,
-            item: item.item,
-            update: item.update
-        };
+
+const Edit = ({ saveEdit, item }) => {
+    const [editItem, setEditItem] = useState({
+        id: item.id,
+        item: item.item,
+        update: item.update
+    })
+
+    const handleChange = ({target: {value, id}}) => {
+        setEditItem({
+            ...editItem,
+            [id]: value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        saveEdit(editItem);
     }
 
-    render() {
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                id='item'
+                value={editItem.item}
+                // placeholder={item.length <= 0 ? 'digite algo para adcionar na lista' : ' '}
+                onChange={handleChange}
+            />
 
-        const { item } = this.state;
-        const handleChange = ({target: {value, id}}) => {
-            this.setState({
-                [id]: value,
-            });
-        };
-
-        const handleSubmit = (event) => {
-            const { saveEdit } = this.props;
-            event.preventDefault();
-
-            saveEdit(this.state);
-        }
-
-        return (
-            <form onSubmit={handleSubmit}>
-                <input
-                    type='text'
-                    id='item'
-                    value={item}
-                    // placeholder={item.length <= 0 ? 'digite algo para adcionar na lista' : ' '}
-                    onChange={handleChange}
-                />
-
-                <button
-                    type='submit'
-                    disabled={item.length <= 0}
-                >
-                    Salvar
-                </button>
-            </form>
-        )
-    }
+            <button
+                type='submit'
+                disabled={item.length <= 0}
+            >
+                Salvar
+            </button>
+        </form>
+    )
 }
+
+// class Edit extends Component {
+    
+//     constructor(props) {
+//         super(props);
+//         const { item } = this.props;
+//         this.state = {
+//             id: item.id,
+//             item: item.item,
+//             update: item.update
+//         };
+//     }
+
+//     render() {
+
+//         const { item } = this.state;
+//         const handleChange = ({target: {value, id}}) => {
+//             this.setState({
+//                 [id]: value,
+//             });
+//         };
+
+//         const handleSubmit = (event) => {
+//             const { saveEdit } = this.props;
+//             event.preventDefault();
+
+//             saveEdit(this.state);
+//         }
+
+//         return (
+//             <form onSubmit={handleSubmit}>
+//                 <input
+//                     type='text'
+//                     id='item'
+//                     value={item}
+//                     // placeholder={item.length <= 0 ? 'digite algo para adcionar na lista' : ' '}
+//                     onChange={handleChange}
+//                 />
+
+//                 <button
+//                     type='submit'
+//                     disabled={item.length <= 0}
+//                 >
+//                     Salvar
+//                 </button>
+//             </form>
+//         )
+//     }
+// }
 
 const mapDispatchToProps = (dispatch) => ({
     saveEdit: (item) => dispatch(saveEdit(item)),
