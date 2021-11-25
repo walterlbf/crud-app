60a5746c8931b935a6e0c12b9ca507ede548e102
walterlbf <walter.lbf.f@gmail.com>
2021-11-25 15:58:12 -0300

    componente Item refatorado para arrow function

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,20 +3,30 @@ import { connect } from 'react-redux';
 import { deleteItem, updateItem } from '../actions';
 
 
-class Item extends Component {
-    render() {
-
-        const { item, toDelete, toUpdate} = this.props;
-        
-        return (
-            <>
-                <p>{item.item}</p>
-                <button onClick={() => toUpdate(item)}>Editar</button>
-                <button onClick={() => toDelete(item.id)}>remover</button>  
-            </>
-        )
-    }
+const Item = ({item, toDelete, toUpdate}) => {
+    return (
+        <>
+            <p>{item.item}</p>
+            <button onClick={() => toUpdate(item)}>Editar</button>
+            <button onClick={() => toDelete(item.id)}>remover</button>  
+        </>
+    )
 }
+
+// class Item extends Component {
+//     render() {
+
+//         const { item, toDelete, toUpdate} = this.props;
+        
+//         return (
+//             <>
+//                 <p>{item.item}</p>
+//                 <button onClick={() => toUpdate(item)}>Editar</button>
+//                 <button onClick={() => toDelete(item.id)}>remover</button>  
+//             </>
+//         )
+//     }
+// }
 
 const mapDispatchToProps = (dispatch) => ({
     toDelete: (id)=> dispatch(deleteItem(id)),
